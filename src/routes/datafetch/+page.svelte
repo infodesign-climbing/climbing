4f2d9628b374bc53f6c54e1d5914eb841939216e
swo98 <swo98@naver.com>
2023-12-13 17:11:05 +0900

    data fetching test

--- src/routes/datafetch/+page.svelte
+++ src/routes/datafetch/+page.svelte
@@ -1,31 +1,98 @@
+<!-- YourMainComponent.svelte -->
+
 <script lang="ts">
-  import { type ChainInfo } from '@src/lib';
+  import { type AssetInfo, type ChainInfo, type ProposalInfo } from '@src/lib';
   import { requestGet } from '@src/lib/requester';
 
-  let data: ChainInfo;
+  let akash: ChainInfo;
+  let assets: AssetInfo;
+  let proposals: ProposalInfo;
 
-  const promiseData = requestGet<ChainInfo>('/data/ChainInfo_final/info_akash.json').then((d) => {
-    data = d;
+  const promiseChainInfoData = requestGet<ChainInfo>('/data/ChainInfo_final/info_akash.json').then((d) => {
+    akash = d;
     return d;
   });
+
+  const promiseAssetsData = requestGet<AssetInfo>('/data/Assets_final/info_akash.json').then((d) => {
+    assets = d;
+    return d;
+  });
+
+  const promiseProposalsData = requestGet<ProposalInfo>('/data/Proposals_final/proposals_akash.json').then((d) => {
+    proposals = d;
+    return d;
+  });
+
 </script>
 
 <div>
-  {#await promiseData}
-    <div>Loading...</div>
-  {:then data}
-  {data.data[0].chain}
-  {data.data[0].recordTime}
-  {data.data[0].txs}
-  {data.data[0].validators.active}
-  {data.data[0].validators.total}
-    {#if data.data && data.data[0] && data.data[0].market_values}
-      {#each data.data[0].market_values as value, index (index)}
-        <p>{index}: {value}</p>
-      {/each}
-    {:else}
-      <p>Market values not available</p>
-    {/if}
+
+  
+  
+  <div> ====== ChainInfo Data ====== </div>
+
+
+  {#await promiseChainInfoData}
+    <div>Loading Akash data...</div>
+  {:then}
+    <!-- Display Akash data -->
+    <div>
+      chain: {akash.data[0].chain}<br/>
+      recordTime: {akash.data[0].recordTime}<br/>
+      txs: {akash.data[0].txs}<br/>
+      validators-active: {akash.data[0].validators.active}<br/>
+      validators-total: {akash.data[0].validators.total}<br/>
+      accounts_monthly: {akash.data[0].accounts_monthly}<br/>
+      market_values: 
+      {#if akash.data && akash.data[0] && akash.data[0].market_values}
+        {#each akash.data[0].market_values as value, index (index)}
+          <p>{value}</p>
+        {/each}
+      {:else}
+        <p>Market values not available</p>
+      {/if}
+      tokens-bonded: {akash.data[0].tokens.bonded}<br/>
+      tokens-supply: {akash.data[0].tokens.supply}<br/>
+      tokens-community: {akash.data[0].tokens.community}<br/>
+    </div>
+  {:catch error}
+    <div>{error.message}</div>
+  {/await}
+
+  
+  <div> ====== Asset Data ====== </div>
+
+  {#await promiseAssetsData}
+    <div>Loading Assets data...</div>
+  {:then}
+    <!-- Display Assets data -->
+    <div>
+      displayDenom : {assets.data[0].assets[0].displayDenom}<br/>
+      tokenImg : {assets.data[0].assets[0].tokenType}<br/>
+      tokenType : {assets.data[0].assets[0].tokenType}<br/>
+    </div>
+  {:catch error}
+    <div>{error.message}</div>
+  {/await}
+
+
+
+  <div> ====== Proposals Data ====== </div>
+
+
+  {#await promiseProposalsData}
+    <div>Loading Proposals data...</div>
+  {:then}
+    <!-- Display Proposals data -->
+    <div>
+      title : {proposals.data[0].title}<br/>
+      voting_start_time : {proposals.data[0].voting_start_time}<br/>
+      voting_end_time : {proposals.data[0].voting_end_time}<br/>
+      voteMeta-Yes: {proposals.data[0].voteMeta.yes}<br/>
+      voteMeta-no: {proposals.data[0].voteMeta.no}<br/>
+      voteMeta-abstain: {proposals.data[0].voteMeta.abstain}<br/>
+  
+    </div>
   {:catch error}
     <div>{error.message}</div>
   {/await}
